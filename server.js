d0115f0fe9577b2acc647681a6efb396ed19f31e
Lizzy Reese <reeseelizabeth25@gmail.com>
2019-11-07 18:59:32 -0500

    fixed proxy error

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ if (process.env.NODE_ENV === "production") {
 
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
 
 
